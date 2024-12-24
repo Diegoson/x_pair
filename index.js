0688dea2bed139527a3c6a15f744e5040fa81f17
💞-ᴺᴀxᴼʀ-💞 <109889758+Diegoson@users.noreply.github.com>
2024-12-24 13:16:21 +0200

    Delete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const pino = require('pino');
 const NodeCache = require('node-cache');
 const { Mutex } = require('async-mutex');
 const crypto = require('crypto');
-const { saveCreds } = require(./mongo');
+const { saveCreds } = require('./mongo');
 const {
     default: makeWASocket,
     useMultiFileAuthState,
@@ -14,29 +14,31 @@ const {
     makeCacheableSignalKeyStore,
     DisconnectReason
 } = require('@whiskeysockets/baileys');
-const { saveSession, getSession, deleteSession } = require('./mongo');
 const app = express();
 const port = 3000;
 let session;
 const msgRetryCounterCache = new NodeCache();
 const mutex = new Mutex();
+const logger = pino({ level: 'silent' });
+const childLogger = logger.child({ level: 'silent', name: 'XAstral' });
 app.use(express.static(path.join(__dirname, 'pages')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'dashboard.html'));
 });
+
 async function connector(Num, res) {
-  const sessionId = `Naxor~${crypto.randomBytes(8).toString('hex')}`;
-  const { state, saveCreds } = await useMultiFileAuthState(
-    "./mongo",
-    pino({ level: "silent" })
-  );
+    const sessionId = `Naxor~${crypto.randomBytes(8).toString('hex')}`;
+    const { state, saveCreds } = await useMultiFileAuthState(
+        "./mongo",
+        childLogger
+    );
     session = makeWASocket({
         auth: {
             creds: state.creds,
-            keys: makeCacheableSignalKeyStore(state.keys, pino({ level: 'fatal' }).child({ level: 'fatal' }))
+            keys: makeCacheableSignalKeyStore(state.keys, childLogger)
         },
         printQRInTerminal: false,
-        logger: pino({ level: 'fatal' }).child({ level: 'fatal' }),
+        logger: childLogger,
         browser: Browsers.macOS("Safari"),
         markOnlineOnConnect: true,
         msgRetryCounterCache
@@ -49,22 +51,21 @@ async function connector(Num, res) {
             res.send({ code: code?.match(/.{1,4}/g)?.join('-') });
         }
     }
+
     session.ev.on('creds.update', async () => {
         await saveCreds();
-        await saveSession(sessionId, state); 
     });
     session.ev.on('connection.update', async (update) => {
         const { connection, lastDisconnect } = update;
         if (connection === 'open') {
-            console.log('Connected successfully');
+            childLogger.info('Connected successfully');
             await delay(5000);
             await session.sendMessage(session.user.id, { text: "*X Astral*:\nDont share_ur_session ID" });
-            console.log('[Session] Session online');
+            childLogger.info('[Session] Session online');
             await session.sendMessage(session.user.id, { text: `${sessionId}` });
         } else if (connection === 'close') {
             const reason = lastDisconnect?.error?.output?.statusCode;
-            console.log(`Connection closed. Reason: ${reason}`);
-            await deleteSession(sessionId); 
+            childLogger.error(`Connection closed. Reason: ${reason}`);
             reconn(reason);
         }
     });
@@ -72,28 +73,32 @@ async function connector(Num, res) {
 
 function reconn(reason) {
     if ([DisconnectReason.connectionLost, DisconnectReason.connectionClosed, DisconnectReason.restartRequired].includes(reason)) {
-        console.log('Connection lost, reconnecting...');
+        childLogger.warn('Connection lost, reconnecting...');
         connector();
     } else {
-        console.log(`Disconnected! Reason: ${reason}`);
+        childLogger.error(`Disconnected! Reason: ${reason}`);
         session.end();
-    }}
+    }
+}
 
 app.get('/pair', async (req, res) => {
     const Num = req.query.code;
     if (!Num) {
-    return res.status(418).json({ message: 'Phone number is required' }); }
+        return res.status(418).json({ message: 'Phone number is required' });
+    }
     const release = await mutex.acquire();
     try {
         await connector(Num, res);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'server Error' });
+        childLogger.error(error);
+        res.status(500).json({ error: 'Server Error' });
     } finally {
         release();
     }
 });
 
 app.listen(port, () => {
-    console.log(`Running on PORT:${port}`);
+    childLogger.info(`PORT: ${port}`);
 });
+
+    
